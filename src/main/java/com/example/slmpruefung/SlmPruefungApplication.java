6eb55deb1b5e0a31d86f6af9ad755a835a42a05b
Lukas Huber <lukashuber2020@gmail.com>
2023-06-13 09:55:40 +0200

    added DigitSum function

--- src/main/java/com/example/slmpruefung/SlmPruefungApplication.java
+++ src/main/java/com/example/slmpruefung/SlmPruefungApplication.java
@@ -2,6 +2,8 @@ package com.example.slmpruefung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class SlmPruefungApplication {
@@ -10,4 +12,13 @@ public class SlmPruefungApplication {
         SpringApplication.run(SlmPruefungApplication.class, args);
     }
 
+    @RequestMapping("/SlmPruefung")
+    public int digitSum(@RequestParam int a) {
+        int sum = 0;
+        while (a > 0) {
+            sum += a % 10;
+            a = a % 10;
+        }
+        return sum;
+    }
 }
