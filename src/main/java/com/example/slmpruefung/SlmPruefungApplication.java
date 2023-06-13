5a4114fa3af5e4cbe476fd5ae8a96d5107957661
Lukas Huber <lukashuber2020@gmail.com>
2023-06-13 10:08:01 +0200

    fixed an issue with the digitSum function

--- src/main/java/com/example/slmpruefung/SlmPruefungApplication.java
+++ src/main/java/com/example/slmpruefung/SlmPruefungApplication.java
@@ -3,17 +3,19 @@ package com.example.slmpruefung;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class SlmPruefungApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SlmPruefungApplication.class, args);
+
     }
 
-    @RequestMapping("/SlmPruefung")
-    public int digitSum(@RequestParam int a) {
+    @RequestMapping("/slmPruefungApplication")
+    @RequestParam(int a)
+    public int digitSum(int a) {
         int sum = 0;
         while (a > 0) {
             sum += a % 10;
