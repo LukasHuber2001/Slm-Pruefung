715516eb981836a18cdfd80d50265c9cd2b314fa
Lukas Huber <lukashuber2020@gmail.com>
2023-06-13 11:05:49 +0200

    added Test for Digit Sum

--- src/test/java/com/example/slmpruefung/SlmPruefungApplicationTests.java
+++ src/test/java/com/example/slmpruefung/SlmPruefungApplicationTests.java
@@ -3,11 +3,19 @@ package com.example.slmpruefung;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SlmPruefungApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
+    @Test
+    void testDigitSum() {
+        SlmPruefungApplication slm = new SlmPruefungApplication();
+        assertEquals(10, slm.countDigitSum(1234));
+    }
 }
